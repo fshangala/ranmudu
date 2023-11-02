7194071e4c8a878e6567084d82de7de9ece61b91
fshangala <fshangala@gmail.com>
2023-11-02 10:58:02 +0200

    update

--- resources/views/frontend/inc/header.blade.php
+++ resources/views/frontend/inc/header.blade.php
@@ -141,7 +141,7 @@
                         </li>
                         @else
                         <li class="list-inline-item ml-4">
-                            <a class="text-reset opacity-60" href="{{ route('login') }}">{{ translate('Log In') }}</a>
+                            <a class="text-reset opacity-60" href="{{ route('user.login') }}">{{ translate('Log In') }}</a>
                         </li>
                         <li class="list-inline-item ml-3">
                             <a class="btn btn-sm bg-primary-grad text-white fw-600 py-1 border"
